43e22f007dc2b920ca4f514e7deb40e4afd2129e
ebenezer <ebenezeroforia08@gmail.com>
2025-10-28 16:27:01 +0000

    fix: moved to js

--- src/routes/adminAuthRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { loginAdmin, registerAdmin } from "../controllers/adminAuthController";
-const router = express.Router()
-
-router.post('/register', registerAdmin)
-router.post('/login', loginAdmin)
-
-export default router;
